96dab67137ff965561010f0890c32421dabf66f0
Andrew McNeill <andrew@andrewmc.ca>
2022-01-10 15:44:07 -0400

    Variation on console.assert that is more colourful and quicker to read

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = (actual, expected) => {
+
+  if (actual === expected) {
+
+    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+
+  } else {
+
+    console.log('⛔️⛔️⛔️ Assertion Failed: ' + actual + ' !== ' + expected);
+
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 1);
