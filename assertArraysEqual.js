4ec06462fc066ec0f9851b71fc0539faecc59eb8
Andrew McNeill <andrew@andrewmc.ca>
2022-01-12 19:28:22 -0400

    Function that returns a modified array that excludes specified values

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -27,7 +27,7 @@ const assertArraysEqual = (array1, array2) => {
     console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
     
   } else {
-    
+
     
     console.log(`⛔️⛔️⛔️ Assertion Failed: [${array1}] !== [${array2}]`);
 
