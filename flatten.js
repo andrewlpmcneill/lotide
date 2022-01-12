c4a8e58b0f76087d055e88a3e0494d2e600fccd6
Andrew McNeill <andrew@andrewmc.ca>
2022-01-11 22:33:03 -0400

    Function that flattens nested arrays inside of a parent array

---
+++ flatten.js
@@ -0,0 +1,23 @@
+const output = [];
+
+const flatten = array => {
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (Array.isArray(array[i])) {
+
+      flatten(array[i]);
+
+    } else {
+
+      output.push(array[i]);
+
+    }
+
+  }
+
+  return output;
+
+};
+
+console.log(flatten([1, 2, [3, [4, 'test', 'test2']], 5, [6]]));
