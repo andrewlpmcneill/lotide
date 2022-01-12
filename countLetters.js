d0f91fa4cd65c22bf8184b2f5078b5d96ac94e6c
Andrew McNeill <andrew@andrewmc.ca>
2022-01-12 16:27:01 -0400

    Function that counts the letters of a strings and returns them as an object

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const countLetters = string => {
+
+  let counts = {};
+  for (let i = 0; i < string.length; i++) {
+
+    if (counts[string[i]] && string[i] !== ' ') {
+
+      counts[string[i]]++;
+
+    } else if (string[i] !== ' ') {
+
+      counts[string[i]] = 1;
+
+    }
+
+  }
+
+  return counts;
+
+};
