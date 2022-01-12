a2aa8e5ea3ad9144ea17032b9b53aef4ea91188a
Andrew McNeill <andrew@andrewmc.ca>
2022-01-12 17:12:18 -0400

    Function that returns the first key in a passed object whose value is equivalent to a passed value

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = (actual, expected) => {
+
+  if (actual === expected) {
+
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+
+    console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+
+};
+
+const findKeyByValue = (object, value) => {
+
+  for (let key in object) {
+
+    if (object[key] === value) {
+
+      return key;
+
+    }
+
+  }
+
+  return undefined;
+
+};
