ab7c5b614de6fe6feec477f8436cd732ecc0d5f2
Andrew McNeill <andrew@andrewmc.ca>
2022-01-17 11:18:59 -0400

    More refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,31 +1,15 @@
-const assertEqual = (actual, expected) => {
-
-  if (actual === expected) {
-
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-
-    console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = (array1, array2) => {
 
   if (array1.length !== array2.length) {
-
     return false;
-
   }
 
   for (let i = 0; i < array1.length; i++) {
-
+    
     if (array1[i] !== array2[i]) {
-
       return false;
-
     }
 
   }
@@ -33,4 +17,6 @@ const eqArrays = (array1, array2) => {
   return true;
 
 };
+
+module.exports = eqArrays;
 
