3bb285d5849083854f07db19b3c694b67af35253
Andrew McNeill <andrew@andrewmc.ca>
2022-01-17 15:15:28 -0400

    Refactoring

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,13 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertArraysEqual(tail([1,2,3,4,"test", 7]), [2,3,4,"test", 7]);
-assertArraysEqual(tail([0,1,2,3,4,"test", 7]), [1,2,3,4,"test", 7]);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  
+  it("returns [1, 2, 3, 4, 'test'] for [0, 1, 2, 3, 4, 'test'", () => {
+    assert.deepEqual(tail([0, 1, 2, 3, 4, 'test']), [1, 2, 3, 4, 'test']);
+  });
+});
