909dd49d9a410c40054054c5df914ae97c3e586d
Andrew McNeill <andrew@andrewmc.ca>
2022-01-18 00:02:00 -0400

    Refactored

--- findKey.js
+++ findKey.js
@@ -1,17 +1,3 @@
-const assertEqual = (actual, expected) => {
-
-  if (actual === expected) {
-
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-
-    console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-
-};
-
 const findKey = (object, callback) => {
 
   for (let key in object) {
@@ -25,18 +11,5 @@ const findKey = (object, callback) => {
   return undefined;
 
 };
-
-const data = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-
-assertEqual(findKey(data, x => x === 2), 'noma');
-assertEqual(findKey(data, x => x === 3), 'Akaleri');
-assertEqual(findKey(data, x => x === 1), 'Blue Hill');
 
 module.exports = findKey;
